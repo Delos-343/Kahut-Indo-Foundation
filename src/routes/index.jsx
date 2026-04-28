5a84d483634975a42ce5c8b8a1ce804548080227
Delos-343 <87126965+Delos-343@users.noreply.github.com>
2026-04-28 18:22:30 +0700

    Removed internal-sensitive pages

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,7 +3,6 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/Home';
 import ContactUs from '../pages/ContactUs';
 import NotFound from '../pages/NotFound';
-import AboutUs from '../pages/About';
 import Experts from '../pages/About/Experts';
 import Governance from '../pages/About/Governance';
 
@@ -12,7 +11,6 @@ import UnderConstruction from '../pages/UnderConstruction';
 import IndustrialCapacity from '../pages/Product/StrategicProduct/Industrial Capacity and Vocational Training';
 import ProgramEcosystem from '../pages/Product/StrategicProduct/Program Ecosystem';
 import WorkersEducation from '../pages/Product/StrategicProduct/Workers Education and Development';
-import FundAsset from '../pages/Product/StrategicProduct/Fund and Asset Management';
 import EcoClimateEnvironment from '../pages/Product/StrategicProduct/Ecolabel  Climate Green Jobs  Environment';
 import LegalAidDisputeResolution from '../pages/Product/StrategicProduct/Legal Aid Dispute Resolution';
 
@@ -20,7 +18,6 @@ function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about-us" element={<AboutUs />} />
             <Route path="/about-us/experts" element={<Navigate to="/expert-teams" replace />} />
             <Route path="/about-us/governance" element={<Navigate to="/our-team" replace />} />
             <Route path="/expert-teams" element={<Experts />} />
@@ -31,7 +28,6 @@ function App() {
             <Route path="/product/strategic-program/ecosystem" element={<ProgramEcosystem />} />
             <Route path="/product/strategic-program/workers-dev" element={<WorkersEducation />} />
             <Route path="/product/strategic-program/industrial-training" element={<IndustrialCapacity />} />
-            <Route path="/product/strategic-program/fund-mgmt" element={<FundAsset />} />
             <Route path="/product/strategic-program/fund-mgmtg" element={<Navigate to="/product/strategic-program/fund-mgmt" replace />} />
             <Route path="/product/strategic-program/ecolabel-env" element={<EcoClimateEnvironment />} />
             <Route path="/product/strategic-program/legal-adr" element={<LegalAidDisputeResolution />} />
